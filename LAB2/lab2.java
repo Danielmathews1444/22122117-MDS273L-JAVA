cf59cc9bb00fab87419192f17abe1e196952e711
Danielmathews1444 <daniel.mathews@msds.christuniversity.in>
2023-02-08 23:13:57 +0530

    update

--- LAB2/lab2.java
+++ LAB2/lab2.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class lab2{
     public static void main(String[] args){
-        int ctrl=1;
+        int ctrl=0;
         int choice;
         String name;
         String[] details = new String[1024];
@@ -16,57 +16,67 @@ public class lab2{
             choice = Integer.parseInt(ob.nextLine());
             switch(choice){
                 case 1:
-                    System.out.println("Enter your name");
+                    System.out.print("Enter your name : ");
                     name=ob.nextLine();
                     int x=0;
                     for(int i=0;i<ctrl;i++){
                         if (name.equals(details[i])){
                             System.out.println("The name already exists");
                             x=i;
+                            break;
                         }
-                        break;
                     }
-                    if(x!=0){
-                        details[x]=name;
+                    if(x==0){
+                        details[ctrl]=name;
                         ctrl++;
-                        System.out.println(ctrl);
-                        break;
                     } 
                     break;
                 case 2:
-                    System.out.println("Enter the name to search");
+                    System.out.print("Enter the name to search : ");
                     name=ob.nextLine();
+                    int z=0;
                     for(int i=0;i<ctrl;i++){
                         if (name.equals(details[i])){
-                            System.out.println(details[i]);
-                            break;
+                            System.out.println("The name is : " + details[i]);
+                            z=1;
                         }
-                        else{
-                            System.out.println("Entered name doesnot exists");
-                            break;
-                        }  
+                        break;
                     }
+                        
+                    if(z==0){
+                        System.out.println("Entered name doesnot exists");
+                    }  
                     break;
                 case 3:
-                    System.out.println("Enter the name to be removed");
+                    System.out.print("Enter the name to be removed : ");
                     name=ob.nextLine();
+                    int k=0;
                     for(int i=0;i<ctrl;i++){
                         if(name.equals(null)){
                             break;
                         }
                         if(name.equals(details[i])){
+                            k=1;
+                        }
+                        if(k==1){
                             details[i]=details[i+1];
+
+                        }
+                        else{
+                     System.out.println("Entered name does not exists");                           
                         }
                     }
                     System.out.println("Entered name has been removed");
                     ctrl-=1;
                     break;
                 case 4:
-                    System.out.println(ctrl);
                     for(int i=0;i<ctrl;i++){
                         System.out.println(details[i]);
                     }
                     break;
+                default:
+                    System.out.println("Invalid option please try again");
+
             }
             System.out.println("press 1 to continue and press 0 to exit");
             choice = Integer.parseInt(ob.nextLine());
