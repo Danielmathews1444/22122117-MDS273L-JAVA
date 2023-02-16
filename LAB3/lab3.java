6b4bfea19d8a00f8d3793fd77bbedafd2fe36ae6
Danielmathews1444 <daniel.mathews@msds.christuniversity.in>
2023-02-16 22:28:05 +0530

    Readme file

--- LAB3/lab3.java
+++ LAB3/lab3.java
@@ -1,90 +1,109 @@
-import java.util.Scanner;
-public class lab3{
-    /////////////////////////////////////
-    static float mean(int[] arr){
-        float z=0;
-        for(int i=0;i<arr.length;i++){
-            z+=arr[i];
-        }
-        float x =  z/arr.length;
-        return  x;
-    }
-    ////////////////////////////////////////////
-    static float median(int[] arr){
-        float median = 0;
-       
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length-i-1; j++) {
-                if(arr[j] >= arr[j+1]){
-                    int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
-                }
-        }
-        }
-        for(int i=0;i<arr.length;i++){
-            if(arr.length % 2==0){
-                float m=(arr[(arr.length/2)-1]+arr[(arr.length/2)]);
-                median = m/2;
-            }else{
-                median = arr[(arr.length-1)/2];
-            }
-        }
-        return median; 
-    }
-    /////////////////////////////////////////////
-    static int mode(int[] arr){
 
-        int max = 0;
-        int mode = 0;
-        for(int i=0;i<arr.length;i++){
-            int count = 0;
-            for(int j=i+1;j<arr.length;j++){
-                if(arr[i]==arr[j]){
-                    count++;
-                }
-            }
-            if(count>max){
-                max=count;
-                mode=arr[i];
-            }
-            }
-        if(max==1){
-            mode=0;
-        }
-        return mode;
+// Importing the Scanner class from the Java Util Library
+import java.util.Scanner;
+public class Lab3 {
+// Function to calculate mean of an array of integers
+static float calculateMean(int[] arr){
+    float sum = 0;
+    for(int i=0; i<arr.length; i++){
+        sum += arr[i];
     }
-    //////////////////////////////////////////////
-    public static void main(String[] args){
-        Scanner scan = new Scanner(System.in);
-        int ctrl = 0;
-        int[] arr = new int[10];
-        for(int i=1;i<arr.length+1;i++){
-            System.out.print("Enter the "+i+"th element of the array : ");
-            arr[i]=Integer.parseInt(scan.nextLine());
+    float mean = sum/arr.length;
+    return mean;
+}
+
+// Function to calculate median of an array of integers
+static float calculateMedian(int[] arr){
+    float median = 0;
+
+    // Sorting the array in ascending order
+    for (int i = 0; i < arr.length; i++) {
+        for (int j = 0; j < arr.length-i-1; j++) {
+            if(arr[j] >= arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
         }
-        do{
-            System.out.println("*****MENU******");
-            System.out.println("1) Mean\n2) Median\n3) Mode");
-            int choice = Integer.parseInt(scan.nextLine());
-            switch(choice){
-                case 1:
-                    System.out.println("The mean of the array is : "+mean(arr));
-                    break;
-                case 2:
-                    System.out.println("The median of the array is : "+median(arr));
-                    break;
-                case 3:
-                    int m=mode(arr);
-                    if(m==0){
-                        System.out.println("The given array has no mode");
-                    }else{
-                        System.out.println("The mode of the array is : "+m);
-                    }
-                    break;
+    }
+
+    // Calculating the median of the array
+    for(int i=0; i<arr.length; i++){
+        if(arr.length % 2 == 0){
+            float m = (arr[(arr.length/2)-1] + arr[(arr.length/2)]);
+            median = m/2;
+        } else {
+            median = arr[(arr.length-1)/2];
+        }
+    }
+
+    return median; 
+}
+
+// Function to calculate mode of an array of integers
+static int calculateMode(int[] arr){
+    int max = 0;
+    int mode = 0;
+
+    for(int i=0; i<arr.length; i++){
+        int count = 0;
+        for(int j=i+1; j<arr.length; j++){
+            if(arr[i] == arr[j]){
+                count++;
             }
-            System.out.print("press 0 to continue \n press any other to exit\nChoice --> ");
-            ctrl=Integer.parseInt(scan.nextLine());
-        }while(ctrl==0);
-            }
+        }
+        if(count > max){
+            max = count;
+            mode = arr[i];
+        }
+    }
+
+    // If no mode is present in the array
+    if(max == 1){
+        mode = 0;
+    }
+
+    return mode;
+}
+
+// Main function
+public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    int ctrl = 0;
+    int[] arr = new int[10];
+
+    // Taking input from the user and storing it in the array
+    for(int i=0; i<arr.length; i++){
+        System.out.print("Enter the " + (i+1) + "th element of the array: ");
+        arr[i] = Integer.parseInt(scan.nextLine());
+    }
+    //menu
+    do {
+        System.out.println("*****MENU******");
+        System.out.println("1) Mean\n2) Median\n3) Mode");
+
+        int choice = Integer.parseInt(scan.nextLine());
+
+        switch(choice) {
+            case 1://mean
+                System.out.println("The mean of the array is: " + calculateMean(arr));
+                break;
+            case 2://median
+                System.out.println("The median of the array is: " + calculateMedian(arr));
+                break;
+            case 3://mode
+                int m = calculateMode(arr);//check if there is a mode or not
+                if(m == 0){
+                    System.out.println("The given array has no mode");
+                } else {
+                    System.out.println("The mode of the array is: " + m);
+                }
+                break;
+        }
+
+        System.out.print("Press 0 to continue.\nPress any other key to exit.\nChoice --> ");
+        ctrl = Integer.parseInt(scan.nextLine());
+
+    } while(ctrl == 0);
+}
 }
---
+++ LAB3/readMe.md
@@ -0,0 +1,23 @@
+Java program to find Mean, Median, and Mode of an Array
+This Java program calculates the Mean, Median, and Mode of an Array of 10 integers. It takes the input from the user and then displays a menu to select the operation to perform.
+
+How to use the program?
+Run the program in a Java compiler or IDE.
+Enter the 10 integers when prompted by the program.
+Select the operation to perform: Mean, Median, or Mode.
+If you want to perform another operation, enter 0 to continue, else enter any other number to exit the program.
+
+The program has the following functions:
+mean(int[] arr): Calculates the mean (average) of the given integer array.
+median(int[] arr): Calculates the median of the given integer array.
+mode(int[] arr): Calculates the mode (most frequent element) of the given integer array.
+
+How does it work?
+The main() function takes input from the user and creates an integer array of size 10.
+The user selects an operation to perform from the menu.
+Depending on the selection, the corresponding function is called to calculate the required value.
+The calculated value is displayed on the console.
+The user is prompted to continue or exit the program.
+
+Assumptions
+The program assumes that the user will only enter 10 integers and will not enter any non-integer values.
